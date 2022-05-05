b69bfd19e190244b6dc7fd5777789e75b5f1d019
Miller-RS <alexander.rojas.s@uni.pe>
2022-05-05 10:15:29 -0500

    1first

---
+++ src/main/java/test/MetodoMainJhon.java
@@ -0,0 +1,33 @@
+package test;
+import java.util.Arrays;
+import java.util.List;
+
+public class MetodoMainJhon {
+
+    public static void main(String[] args) {
+        test1();
+        //test2();
+    }
+
+    private static void test1() {
+        List<String> developers = new PlanningPokerJhon().identifyExtremes(
+                Arrays.asList(
+                        new Estimate("Mauricio", 16),
+                        new Estimate("Frank", 8),
+                        new Estimate("Cesar", 2),
+                        new Estimate("Andy", 4)));
+
+        System.out.println(developers);
+    }
+
+    private static void test2() {
+        List<String> developers = new PlanningPokerJhon().identifyExtremes(
+                Arrays.asList(
+                        new Estimate("Ross", 2),
+                        new Estimate("Phoebe", 4),
+                        new Estimate("Monica", 8),
+                        new Estimate("Chandler", 16)));
+
+        System.out.println(developers);
+    }
+}
